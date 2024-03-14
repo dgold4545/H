0317e2b1f4e2c60cfb71d5b42a75c96bacf68da1
DGOLD7788 <80936904545dgold@gmail.com>
2024-03-14 18:59:39 +0200

    mobile layout

--- js/script.js
+++ js/script.js
@@ -8,3 +8,14 @@ document.querySelector('.button').addEventListener('click', function () {
 document.querySelector('.close-btn').addEventListener('click', function () {
   document.querySelector('.backdrop').classList.toggle('is-open');
 });
+
+
+
+
+document.querySelector('.menu-btn').addEventListener('click', function () {
+  document.querySelector('.backdrop-menu').classList.toggle('is-open');
+});
+
+document.querySelector('.close-btn-menu').addEventListener('click', function () {
+  document.querySelector('.backdrop-menu').classList.toggle('is-open');
+});
